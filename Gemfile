af36c3e3a5ec7a609aaf77becdb5fe5b9558bdac
Greg Terrono <gterrono@seas.upenn.edu>
2012-08-11 04:50:50 -0400

    Added datamapper and some haml

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ source :rubygems
 gem 'sinatra', '1.1.0'
 gem 'thin'
 gem 'pg', '0.10.0'
+gem 'haml'
+gem 'data_mapper'
+gem 'dm-postgres-adapter', :group => :production
+gem 'dm-sqlite-adapter', :group => :development
--- coursegrapher.rb
+++ coursegrapher.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
+require 'data_mapper'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/cg.db")
+
+class Course
+  include DataMapper::Resource
+  property :id, Serial
+  property :name, String
+  property :rating, Float
+end
+
+DataMapper.auto_upgrade!
 
 get '/' do
-  "Hello, there"
+  haml :index
 end
