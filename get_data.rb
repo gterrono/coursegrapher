c13f9ffa7f6e9e4f3e2e0d0e2bdceb2172ffebf5
Greg Terrono <gterrono@seas.upenn.edu>
2012-08-16 03:34:29 -0400

    Added a file that puts the necessary data in a database

---
+++ get_data.rb
@@ -0,0 +1,69 @@
+require 'json'
+require 'net/http'
+require 'data_mapper'
+require 'models'
+
+def get_uri(path)
+  uri_start = 'http://api.penncoursereview.com/v1'
+  token = '?token=asdlkja4lks94nklvm34lkdf09lkns'
+  URI("#{uri_start}#{path}#{token}")
+end
+
+def get_json(path)
+  uri = get_uri(path)
+  response = Net::HTTP.get_response(uri)
+  JSON.parse(response.body)
+end
+
+def get_data()
+  data = get_json('/depts')
+  depts = data['result']['values']
+  depts.each do |dept|
+    process_dept(dept)
+  end
+end
+
+def process_dept(dept)
+  data = get_json(dept['path'])
+  courses = data['result']['coursehistories']
+  department = Department.create(:name => dept['id'])
+  courses.each do |course|
+    department.add process_course(course)
+  end
+end
+
+$key_dict = {
+  :access_inst => "rInstructorAccess",
+  :amount_learned => "rAmountLearned",
+  :amount_work => "rWorkRequired",
+  :course_quality => "rCourseQuality",
+  :difficulty => "rDifficulty",
+  :inst_quality => "rInstructorQuality",
+  :inst_communication => "rCommAbility",
+  :rec_majors => "rRecommendMajor",
+  :rec_non_majors => "rRecommendNonMajor",
+  :stim_int => "rStimulateInterest",
+  :val_of_readings => "rReadingsValue"
+}
+
+def process_course(course)
+  data = get_json("#{course['path']}/reviews")
+  reviews = data['result']['values']
+  compilation = {:num_reviews => 0}
+  $key_dict.each_key {|k| compilation[k] = 0.0}
+  num = 0
+  reviews.each {|review| num = process_review(review, compilation, num)}
+  return nil if num == 0
+  compilation.each_key {|k| compilation[k]/=num}
+  compilation[:name] = course['aliases'][0]
+  Course.create(compilation)
+end
+
+def process_review(review, compilation, num)
+  return num if review['num_reviewers'].nil?
+  ratings = review['ratings']
+  $key_dict.each_value {|v| return num if ratings[v].nil?}
+  compilation[:num_reviews] += review['num_reviewers']
+  $key_dict.each {|k, v| compilation[k] += ratings[v].to_f}
+  num + 1
+end
