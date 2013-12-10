6c11878756511eae42272685a417ee36fdfe5bf1
Greg Terrono <terronogr@gmail.com>
2013-12-10 12:51:36 -0500

    Getting ready to reimplement in coffeescript with firebase

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: bundle exec ruby coursegrapher.rb -p $PORT
--- coursegrapher.rb
+++
@@ -1,8 +0,0 @@
-require 'sinatra'
-require 'data_mapper'
-require_relative 'models'
-
-get '/' do
-  @depts = Department.all.select{|dept| dept.num_reviews != 0}
-  haml :index
-end
